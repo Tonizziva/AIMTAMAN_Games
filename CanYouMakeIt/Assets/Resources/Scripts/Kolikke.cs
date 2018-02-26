8b4d7beea7e04a7b990b818b2990e7617d07d883
Toni <a1503035@myy.haaga-helia.fi>
2018-02-26 17:18:17 +0200

    Kolikko

--- CanYouMakeIt/Assets/Resources/Scripts/Kolikke.cs
+++ CanYouMakeIt/Assets/Resources/Scripts/Kolikke.cs
@@ -7,6 +7,10 @@ public class Kolikke : MonoBehaviour {
 	public Text money;
 	public static int currentMoney = 0;
 
+	void Start() {
+		money.text = currentMoney.ToString();
+	}
+
 	void OnTriggerEnter2D (Collider2D col) {
 		currentMoney = currentMoney + 1;
 		money.text = currentMoney.ToString();
