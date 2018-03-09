a63880280f8f017652c158b72ababaef99fbc347
Toni <a1503035@myy.haaga-helia.fi>
2018-03-09 21:31:44 +0200

    level2 yritys epäonnistui

--- CanYouMakeIt/Assets/Resources/Scripts/LevelEditor.cs
+++ CanYouMakeIt/Assets/Resources/Scripts/LevelEditor.cs
@@ -3,12 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelEditor : MonoBehaviour {
-	public GameObject este0;
-	public GameObject este1;
-	public GameObject este2;
-	public GameObject este3;
-	public GameObject este4;
-	public GameObject este5;
 	public GameObject esteet;
 	public GameObject kolikot;
 
@@ -18,7 +12,7 @@ public class LevelEditor : MonoBehaviour {
 			int r = Random.Range (1, 8);
 			float r2 = Random.Range (-2, 2);
 
-			(Instantiate(Resources.Load("Prefabs/Este"+r),	new Vector2(0.7f, (i+1)*4), Quaternion.identity)
+			(Instantiate(Resources.Load("Prefabs/Level001/Este"+r),	new Vector2(0.7f, (i+1)*4), Quaternion.identity)
 				as GameObject).transform.parent = esteet.transform;
 
 			(Instantiate(Resources.Load("Prefabs/kolikkoryhmä"),	new Vector2(r2, (i+2)*4), Quaternion.identity)
