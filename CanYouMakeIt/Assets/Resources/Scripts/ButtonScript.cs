49aa689bf0a614e8d2a38b32005ccde8102cf907
Toni <a1503035@myy.haaga-helia.fi>
2018-03-13 19:09:35 +0200

    Vähän kaikkee hyväää
    nappeja sun muuta roinaa

---
+++ CanYouMakeIt/Assets/Resources/Scripts/ButtonScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour {
+
+	public void Back()
+    {
+        SceneManager.LoadScene("Menu_Start");
+    }
+
+    public void Leves()
+    {
+        SceneManager.LoadScene("Menu_levels");
+    }
+
+    public void Shop()
+    {
+        SceneManager.LoadScene("Menu_Shop");
+    }
+
+    public void Launch()
+    {
+        SceneManager.LoadScene("Level_001");
+    }
+
+}
